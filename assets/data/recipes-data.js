386539441a8a2fe51644b3502004b9683cf702f8
Alvaro Moreno Lumbreras <amorenolumbreras@gmail.com>
2026-09-14 20:40:49 +0200

    Replace carbonara photo with Trastevere presentation

--- assets/data/recipes-data.js
+++ assets/data/recipes-data.js
@@ -320,7 +320,7 @@ window.RECIPES_DATA = [
     "profile": "salado",
     "difficulty": "media",
     "prepTime": 30,
-    "image": "assets/images/recipes/carbonara-romana.jpg",
+    "image": "assets/images/recipes/carbonara-trastevere.jpg",
     "ingredients": [
       "Para 2 personas: 200 g de pasta (por ejemplo, spaghetti secos)",
       "100-120 g de guanciale",
